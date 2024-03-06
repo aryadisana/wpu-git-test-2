d0820a391a35c8a501efbf140849b9685550e957
aryadisana <aryadimas@apps.ipb.ac.id>
2024-03-06 08:16:04 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, aspernatur sed doloribus ut maiores voluptates sunt suscipit itaque excepturi, impedit ducimus quis, delectus voluptatem voluptatibus sit quaerat earum ipsa quae.</p>
 
 </body>
 
